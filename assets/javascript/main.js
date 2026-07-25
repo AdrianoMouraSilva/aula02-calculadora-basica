38c567e1f618aa268d9947d37b18d0e98c406ebe
Adriano Moura <adr.moura21@hotmail.com>
2026-07-25 15:57:39 -0300

    Função usando case

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,37 +1,86 @@
 'use strict'
+const $ = (seletor) => document.querySelector(seletor);       
+const $$ = (seletor) => document.querySelectorAll(seletor);
 
-// 1. Definição dos atalhos (Shorthands)
-const $ = (seletor) => document.querySelector(seletor);        // Retorna 1 elemento (ID ou Classe)
-const $$ = (seletor) => document.querySelectorAll(seletor);    // Retorna TODOS os elementos
+const botaoSomar = $('#botao-somar')
+const botaoSubtrair = $('#botao-subtrair')
+const botaoMultiplicar = $('#botao-multiplicar')
+const botaoDividir = $('#botao-dividir')
 
-// const botaoCalcular = document.getElementById('botao-calcular')
-const botaoAcao = $('.botao-calcular')
-// const botaoCalcular = $('#botao-subtrair')
-// const botaoCalcular = $('#botao-multiplicar')
-// const botaoCalcular = $('#botao-dividir')
+// 1. Seleciona todos os botões da classe
+// const botoesCalcular = document.querySelectorAll('.botao-calcular');
+const botoesCalcular = $$('.botao-calcular')
 
-function mostrarComissao(){
+function calcularGeral(event) {
+  const acao = event.currentTarget.dataset.acao;
+  const valUm = $('#val-1')
+  const valDois = $('#val-2')
+  const resultado = $('#resultado')
 
-    // Usando o seu atalho $ do querySelector:
-const caixaNome = $('.botao-calcular');
+  switch (acao) {
+    case 'somar':
+        const soma = Number(valUm.value) + Number(valDois.value)
+        resultado.textContent = `${valUm.value} + ${valDois.value} = ${soma}` 
+        break;
 
-// Captura o valor do atributo data-acao:
-const acao = caixaNome.dataset.acao;
+    case 'subtrair':
+        const subtrair = Number(valUm.value) - Number(valDois.value)
+        resultado.textContent = `${valUm.value} - ${valDois.value} = ${subtrair}` 
+        break;
+    case 'multiplicar':
+        const multiplicar = Number(valUm.value) * Number(valDois.value)
+        resultado.textContent = `${valUm.value} x ${valDois.value} = ${multiplicar}` 
+        break;
 
-console.log(`acao ${acao}`);
-
-    // const nome = $('#caixa-nome')
-    // const valor = $('#caixa-valor')
-    // const resultado = $('#resultado')
-
-    // const salario = valor.value * 0.08
-
-    // resultado.textContent = `${nome.value} o valor das venda é: ${Number(valor.value).toFixed(2)} se seu salário é ${Number(salario).toFixed(2)}` 
-    // console.log(nome.value);
-    // console.log(valor.value);
-    // console.log(salario);
+      case 'dividir':
+        const dividir = Number(valUm.value) / Number(valDois.value)
+        resultado.textContent = `${valUm.value} / ${valDois.value} = ${dividir}` 
+        break;
+    default:
+        console.warn('Ação não reconhecida:', acao);
+        break;
+  }
 }
 
-botaoAcao.addEventListener('click', mostrarComissao)
+botoesCalcular.forEach((botao) => {
+  botao.addEventListener('click', calcularGeral);
+});
+
+function calcularSomar(){
+    const valUm = $('#val-1')
+    const valDois = $('#val-2')
+    const resultado = $('#resultado')
+    const valResultado = Number(valUm.value) + Number(valDois.value)
+    // resultado.textContent = `${valUm.value} + ${valDois.value} = ${valResultado}` 
+}
+
+function calcularSubtrair(){
+    const valUm = $('#val-1')
+    const valDois = $('#val-2')
+    const resultado = $('#resultado')
+    const valResultado = Number(valUm.value) - Number(valDois.value)
+    // resultado.textContent = `${valUm.value} - ${valDois.value} = ${valResultado}` 
+}
+
+function calcularMultiplicar(){
+    const valUm = $('#val-1')
+    const valDois = $('#val-2')
+    const resultado = $('#resultado')
+    const valResultado = Number(valUm.value) * Number(valDois.value)
+    // resultado.textContent = `${valUm.value} X ${valDois.value} = ${valResultado}` 
+}
+
+function calcularDividir(){
+    const valUm = $('#val-1')
+    const valDois = $('#val-2')
+    const resultado = $('#resultado')
+    const valResultado = Number(valUm.value) / Number(valDois.value)
+    // resultado.textContent = `${valUm.value} / ${valDois.value} = ${valResultado}` 
+}
+
+botaoSomar.addEventListener('click', calcularSomar)
+botaoSubtrair.addEventListener('click', calcularSubtrair)
+botaoMultiplicar.addEventListener('click', calcularMultiplicar)
+botaoDividir.addEventListener('click', calcularDividir)
 
 
